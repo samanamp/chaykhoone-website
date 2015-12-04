57c217cec38a8ee7a7f1fbe6248765ddf0a24020
samancro <samanamp@outlook.com>
2015-12-04 12:11:31 +0330

    Theme added

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+@section('title', 'چایخـــــونه')
+@section('subtitle', 'یه خونه دنج برای علاقمندان به چای و (بقیه نوشیدنیها)')
+@section('intro-header-image','img/home-bg.jpg')
+
+@section('heading', 'یک چای دبش')
+
+@section('content')
+    <p>این یکی از بهترین انواع چای موجود است.</p>
+@stop
